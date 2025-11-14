9ed121f1caa878d1415dbdc44223817ad71c5fa1
Eliezer Ribeiro <146236086+eliezershell@users.noreply.github.com>
2025-11-14 16:29:25 -0300

    Update base-zabbix.sql

--- init/base-zabbix.sql
+++ init/base-zabbix.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE basezabbix;
+CREATE DATABASE basezabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE USER 'user-zabbix'@'%' IDENTIFIED BY 'pass-zabbix';
 GRANT ALL PRIVILEGES ON base-zabbix.* TO 'user-zabbix'@'%';
 SET GLOBAL log_bin_trust_function_creators = 1;
